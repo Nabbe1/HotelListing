c0e1d29cdf505220368c0350be48c33c4edcd2be
Nabil Benali <nabilbenali135@gmail.com>
2021-12-13 13:21:35 +0100

    Add project files.

---
+++ HotelListing/Data/DatabaseContext.cs
@@ -0,0 +1,69 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {}
+
+       
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Finland",
+                    ShortName = "FN"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Morocco",
+                    ShortName = "MR"
+                }
+            );
+
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Helsinki Resort and Spa",
+                    Address = "Tegelviksplan 1",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Hotel Alazera",
+                    Address = "Agadir",
+                    CountryId = 2,
+                    Rating = 5.0
+                }
+            );
+        }
+    }
+}
---
+++ HotelListing/Migrations/20211213121222_AddedNewCountry.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class AddedNewCountry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "ShortName" },
+                values: new object[] { "Finland", "FN" });
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Name", "ShortName" },
+                values: new object[] { "Morocco", "MR" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Address", "Name" },
+                values: new object[] { "Tegelviksplan 1", "Helsinki Resort and Spa" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Address", "Name", "Rating" },
+                values: new object[] { "Agadir", "Grand Hotel Alazera", 5.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Name", "ShortName" },
+                values: new object[] { "Jamaica", "JM" });
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Name", "ShortName" },
+                values: new object[] { "Cayman Island", "CI" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Address", "Name" },
+                values: new object[] { "Negril", "Sandals Resort and Spa" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Address", "Name", "Rating" },
+                values: new object[] { "Nassua", "Grand Palldium", 4.0 });
+        }
+    }
+}
